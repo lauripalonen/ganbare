bf13ab19ced87e1bcecfb9942b2c45ec3c0019ad
Mieskalmari <palonen.lauri@gmail.com>
2019-03-22 16:46:49 +0200

    Tee osa 3: testauskattavuus

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,60 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void kortilleLatautuuOikeaMaaraRahaa() {
+        kortti.lataaRahaa(10);
+        
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    
+    @Test
+    public void kortiltaVoiOttaaRahaa() {
+        kortti.lataaRahaa(300);
+        
+        kortti.otaRahaa(240);
+        
+        assertEquals("saldo: 0.70", kortti.toString());
+    }
+    
+    @Test
+    public void kortinSaldoEiMeneNegatiiviseksi() {
+        kortti.otaRahaa(240);
+        
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void korttiPalauttaaTrueKunSaldoaOnTarpeeksi () {
+        boolean maksu = kortti.otaRahaa(5);
+        
+        assertEquals(true, maksu);    
+    }
+    
+    @Test
+    public void korttiPalauttaaFalseKunSaldoEiRiita () {
+        boolean maksu = kortti.otaRahaa(15);
+        
+        assertEquals(false, maksu);
+    }
+    
+    @Test
+    public void saldoPieneneeKunRahaaOnTasmalleenOikeaMaara () {
+        kortti.otaRahaa(10);
+        
+        assertEquals("saldo: 0.0", kortti.toString());        
+    }
+    
+    @Test
+    public void korttiPalauttaaTrueKunRahaaOnTasmalleenOikeaMaara () {
+        boolean maksu = kortti.otaRahaa(10);
+        
+        assertEquals(true, maksu);
+    }
 }
